5ca69465097118796aec3207f013ca34701f0c28
developer-cmd-dev <devkmandal0@gmail.com>
2025-01-30 21:29:37 +0530

    Add logic of players name, toss , take choice of toss winner and show user choice in game ui

--- src/Game.java
+++ src/Game.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 
 public class Game {
 
-    private char[][] choiceArr = {
-            {'0', '1', '2'},
-            {'3', '4', '5'},
-            {'6', '7', '8'},
+    private String[][] choiceArr = {
+            {"0", "1", "2"},
+            {"3", "4", "5"},
+            {"6", "7", "8"},
     };
 
-    private String playerOneChoice;
-    private String playerTwoChoice;
+    private String firstPlayerChoice;
+    private String secondPlayerChoice;
+    private String playersChoiceArr[] = {firstPlayerChoice, secondPlayerChoice};
 
 
     Scanner sc;
@@ -31,47 +32,93 @@ public class Game {
         String firstPlayerName = sc.nextLine();
         System.out.println("Enter Second player Name:");
         String secondPlayerName = sc.nextLine();
+        String playersArr[]={firstPlayerName,secondPlayerName};
         System.out.println("Can i do Toss ? (Y/N)");
         String tossDecison = sc.nextLine();
+        String tossWinPlayer;
         int tossNo = 0;
         if (tossDecison.equalsIgnoreCase("Y")) {
             tossNo = (Math.random() <= 0.5) ? 1 : 2;
             switch (tossNo) {
-                case 1 -> System.out.println(firstPlayerName + " won the toss.");
-                case 2 -> System.out.println(secondPlayerName + " won the toss.");
+                case 1 -> {
+                    System.out.println(firstPlayerName + " won the toss.");
+                    tossWinPlayer = firstPlayerChoice;
+                }
+                case 2 -> {
+                    System.out.println(secondPlayerName + " won the toss.");
+                    tossWinPlayer = secondPlayerChoice;
+                }
             }
         } else if (tossDecison.equalsIgnoreCase("N")) {
             System.out.println("Toss canceled.");
+            return;
         } else {
             System.out.println("You entered wrong input.");
+            return;
         }
-        System.out.println("Which Option Do you want to choose: X or O ");
+        System.out.println("Hey winner which option Do you want to choose: X or O ");
         String choiceChar = sc.nextLine();
         if (choiceChar.equalsIgnoreCase("O")) {
+            for (int i = 0; i < playersChoiceArr.length; i++) {
+                if ((tossNo - 1) == i) {
+                    playersChoiceArr[i] = "O";
+                } else {
+                    playersChoiceArr[i] = "X";
+                }
+            }
+        } else if (choiceChar.equalsIgnoreCase("X")) {
+            for (int i = 0; i < playersChoiceArr.length; i++) {
+                if ((tossNo - 1) == i) {
+                    playersChoiceArr[i] = "X";
+                } else {
+                    playersChoiceArr[i] = "O";
+                }
+            }
+        } else {
+            System.out.println("You chose wrong option!");
+        }
+        System.out.println(firstPlayerName + " has chosen - " + playersChoiceArr[0]);
+        System.out.println(secondPlayerName + " has chosen - " + playersChoiceArr[1]);
+        System.out.println("Lets play this game........");
+
+        int choice;
+        int turns[]={0,1};
+        System.out.println(playersChoiceArr[tossNo-1]);
+        while (true){
+            for (int i = 0; i < choiceArr.length; i++) {
+                for (int j = 0; j < 3; j++) {
+                    switch (j) {
+                        case 0 -> {
+                            System.out.print("    " + choiceArr[i][j] + "   ");
+                        }
+                        case 1 -> {
+                            System.out.print("|   " + choiceArr[i][j] + "   |");
+                        }
+                        case 2 -> {
+                            System.out.print("   " + choiceArr[i][j] + "    ");
+                        }
+                    }
+                }
+                System.out.println();
+                if (i == 0 || i == 1) {
+                    System.out.println("--------+-------+---------");
+                }
+
+            }
+            System.out.println(playersArr[tossNo-1]+" Enter indexing No: ");
+            choice = sc.nextInt();
+            sc.nextLine();
+            for (int i = 0; i < choiceArr.length; i++) {
+                for (int j = 0; j <choiceArr.length ; j++) {
+                    if(choiceArr[i][j].equalsIgnoreCase(String.valueOf(choice))){
+
+                        choiceArr[i][j]=playersChoiceArr[0];
+                    }
+                }
+            }
 
         }
 
-
-//        for (int i = 0; i < choiceArr.length; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                switch (j){
-//                    case 0 ->{
-//                        System.out.print("    "+choiceArr[i][j]+"   ");
-//                    }
-//                    case 1 ->{
-//                        System.out.print("|   "+choiceArr[i][j]+"   |");
-//                    }
-//                    case 2->{
-//                        System.out.print("   "+choiceArr[i][j]+"    ");
-//                    }
-//                }
-//            }
-//            System.out.println();
-//            if(i==0 || i==1){
-//                System.out.println("--------+-------+---------");
-//            }
-//
-//        }
 
 
     }
